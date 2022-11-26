934cc44ee28cca24d86225d676d1a0aeb2bf62bc
Ke-Naruse <naruse.ke@sms-datatech.co.jp>
2022-11-27 00:10:42 +0900

    aaaa

--- history_back_test/src/main/java/jp/co/smsdatatech/gittest/HistoryBackTestApplication.java
+++ history_back_test/src/main/java/jp/co/smsdatatech/gittest/HistoryBackTestApplication.java
@@ -2,26 +2,11 @@ package jp.co.smsdatatech.gittest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HistoryBackTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HistoryBackTestApplication.class, args);
-	}
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-
-		return new WebMvcConfigurer() {
-
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://172.31.208.1");
-			}
-		};
 	}
 }
