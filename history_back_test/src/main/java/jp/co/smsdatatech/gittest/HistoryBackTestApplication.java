3285c6c59c9d1673f7210b033ddad5efdf64966c
Ke-Naruse <naruse.ke@sms-datatech.co.jp>
2022-11-26 22:56:37 +0900

    初期設定

---
+++ history_back_test/src/main/java/jp/co/smsdatatech/gittest/HistoryBackTestApplication.java
@@ -0,0 +1,13 @@
+package jp.co.smsdatatech.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryBackTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryBackTestApplication.class, args);
+	}
+
+}
---
+++ history_back_test/src/test/java/jp/co/smsdatatech/gittest/HistoryBackTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.smsdatatech.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryBackTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
